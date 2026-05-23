16345f5985a30c2ec995251e630db44ebfd69dfb
abottleofvodka123-alt <abottleofvodka123@gmail.com>
2026-05-23 11:51:26 +0530

    remove psexec

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 set GROQ_API_KEY=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
 pip install -r requirements.txt -q
-PsExec64.exe -s -i python mcq_solver_free.py
+python mcq_solver_free.py
 pause
