567976124069e5f9aebcda7786fa383c4e886565
abottleofvodka123-alt <abottleofvodka123@gmail.com>
2026-06-02 02:29:34 +0530

    force new exe

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-set GROQ_API_KEY=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
+set GROQ_API_KEY=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
 dist\mcq_solver_free.exe
 pause
